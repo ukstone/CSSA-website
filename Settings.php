53cc402f3d4d97cdf947fd87258cfb2632344ff8
Qingqi Shi <ukstone@qq.com>
2013-11-17 03:09:13 +0800

    Configure local server

--- Settings.php
+++ Settings.php
@@ -14,33 +14,33 @@
 ########## Maintenance ##########
 # Note: If $maintenance is set to 2, the forum will be unusable!  Change it to 0 to fix it.
 $maintenance = 0;		# Set to 1 to enable Maintenance Mode, 2 to make the forum untouchable. (you'll have to make it 0 again manually!)
-$mtitle = '正在维修';		# Title for the Maintenance Mode message.
-$mmessage = '本论坛正在测试中！请耐心等待。';		# Description of why the forum is in maintenance mode.
+$mtitle = 'Maintenance Mode';		# Title for the Maintenance Mode message.
+$mmessage = 'Okay faithful users...we\'re attempting to restore an older backup of the database...news will be posted once we\'re back!';		# Description of why the forum is in maintenance mode.
 
 ########## Forum Info ##########
-$mbname = '诺丁汉学联论坛';		# The name of your forum.
+$mbname = '诺丁汉中国学联';		# The name of your forum.
 $language = 'chinese_simplified-utf8';		# The default language file set for the forum.
-$boardurl = 'http://jian.webatu.com/CSSA';		# URL to your forum's folder. (without the trailing /!)
+$boardurl = 'http://localhost/CSSA-website';		# URL to your forum's folder. (without the trailing /!)
 $webmaster_email = 'ukstone@qq.com';		# Email address to send emails from. (like noreply@yourdomain.com.)
-$cookiename = 'SMFCookie715';		# Name of the cookie to set for authentication.
+$cookiename = 'SMFCookie160';		# Name of the cookie to set for authentication.
 
 ########## Database Info ##########
 $db_type = 'mysql';
-$db_server = 'mysql12.000webhost.com';
-$db_name = 'a6737183_CSSA';
-$db_user = 'a6737183_ukstone';
+$db_server = 'localhost';
+$db_name = 'smf_db';
+$db_user = 'admin';
 $db_passwd = 'woaini344-1107';
 $ssi_db_user = '';
 $ssi_db_passwd = '';
 $db_prefix = 'smf_';
 $db_persist = 0;
-$db_error_send = 0;
+$db_error_send = 1;
 
 ########## Directories/Files ##########
 # Note: These directories do not have to be changed unless you move things.
-$boarddir = '/home/a6737183/public_html/CSSA';		# The absolute path to the forum's folder. (not just '.'!)
-$sourcedir = '/home/a6737183/public_html/CSSA/Sources';		# Path to the Sources directory.
-$cachedir = '/home/a6737183/public_html/CSSA/cache';		# Path to the cache directory.
+$boarddir = 'C:\\xampp\\htdocs\\CSSA-website';		# The absolute path to the forum's folder. (not just '.'!)
+$sourcedir = 'C:\\xampp\\htdocs\\CSSA-website/Sources';		# Path to the Sources directory.
+$cachedir = 'C:\\xampp\\htdocs\\CSSA-website/cache';		# Path to the cache directory.
 
 ########## Error-Catching ##########
 # Note: You shouldn't touch these settings.
